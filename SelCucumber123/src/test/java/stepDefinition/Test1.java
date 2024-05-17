b2bae7f6473a2fa021f4228dc0eef47bfc9ac56b
gouravkumarlaha123 <gouravkumar.laha@gmail.com>
2024-05-17 17:17:19 +0530

    vghgygy

--- SelCucumber123/src/test/java/stepDefinition/Test1.java
+++ SelCucumber123/src/test/java/stepDefinition/Test1.java
@@ -18,7 +18,7 @@ public class Test1 {
 	   driver=new ChromeDriver();
 	   driver.manage().window().maximize();
 	   driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	   driver.get("https://www.google.com/");
+	   driver.get("https://www.google.co.in/");
 	}
 
 	@Then("^Title is Google$")
